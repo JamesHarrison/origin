fa5a112ca416632e79d4bd2c8986c83339ba5231
James Harrison <JamesHarrison@users.noreply.github.com>
2014-03-28 17:56:25 +0000

    Update optional.rb

--- lib/origin/optional.rb
+++ lib/origin/optional.rb
@@ -118,6 +118,7 @@ module Origin
     end
 
     # Limits the results to only contain the fields provided.
+    # Chainable; will overwrite all without limitations.
     #
     # @example Limit the results to the provided fields.
     #   optional.only(:name, :dob)
@@ -131,7 +132,7 @@ module Origin
       args = args.flatten
       option(*args) do |options|
         options.store(
-          :fields, args.inject(options[:fields] || {}){ |sub, field| sub.tap { sub[field] = 1 }}
+          :fields, args.inject(options[:fields].select{|k,v|v==1} || {}){ |sub, field| sub.tap { sub[field] = 1 }}
         )
       end
     end
